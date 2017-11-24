00e17490e310112cdea0575d54a0680e04294027
aishwaryapothula <aishwarya.pothula@gmail.com>
2017-11-25 02:09:13 +0530

    odd even

---
+++ odd_even.py
@@ -0,0 +1,6 @@
+#find out if a number is odd or even
+num=int(input("enter a number:\n"))
+if(num%2==0):
+    print("{} is an even number".format(num))
+else:
+    print("{} is an odd number".format(num))
