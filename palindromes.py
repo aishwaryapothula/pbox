16c22a74a92d9c8422ea052eb02104c47a1ecc3a
aishwaryapothula <aishwarya.pothula@gmail.com>
2017-11-27 23:05:50 +0530

    27nov

---
+++ palindromes.py
@@ -0,0 +1,12 @@
+#AishwaryaPothula
+#To find out whether a word is a palindrome
+
+a=input("enter a word\n") #raw_input if executing in python2
+b=a[::-1]
+if a==b:
+    print("is a palindrome")
+else:
+    print("is not a palindrome")
+
+#takes a word as input. Use raw_input to consider entered input as string if running in python2
+#a[::-1] slices the string in the reverse order
