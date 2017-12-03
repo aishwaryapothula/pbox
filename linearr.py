5b689652f971edba4803d760d9072325b829e029
aishwaryapothula <aishwarya.pothula@gmail.com>
2017-12-03 23:34:36 +0530

    edit1

---
+++ linearr.py
@@ -0,0 +1,11 @@
+#AishwaryaPothula
+#To apply linear regression
+
+import random
+keys=["brain","body"]
+x=1
+while(x<11):
+    dict={keys[0]:random.randint(0,101),keys[1]:random.randint(0,101)}
+    x+=1
+    print dict
+    
