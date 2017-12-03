da38e06909886d62eb5aa58a36264b083175a841
aishwaryapothula <aishwarya.pothula@gmail.com>
2017-12-04 02:11:04 +0530

    edit2

--- linearr.py
+++ linearr.py
@@ -2,10 +2,44 @@
 #To apply linear regression
 
 import random
-keys=["brain","body"]
-x=1
-while(x<11):
-    dict={keys[0]:random.randint(0,101),keys[1]:random.randint(0,101)}
-    x+=1
-    print dict
-    
+import math
+
+x=[]
+y=[]
+i=1
+z=0
+x2=[]
+xy=[]
+
+while(i<=5):
+    a=random.randint(0,101)
+    b=random.randint(0,101)
+    x.append(a)
+    y.append(b)
+    dict={"body":a,"brain":b}
+    print(dict)
+    i+=1
+print("\n{} independent variables".format(x))
+print("{} dependent variables".format(y))
+
+while(z<len(x)):
+    x2.append(x[z]**2)
+    xy.append(x[z]*y[z])
+    z+=1
+print("\n{} x sqaures".format(x2))
+print("{} xy".format(xy))
+
+ce1=((sum(y)*sum(x2))-(sum(x)*sum(xy)))/((i*sum(x2))-(sum(x)**2))
+ce2=((i*sum(xy))-(sum(x)*sum(y)))/((i*sum(x2))-(sum(x)**2))
+print("\n{}".format(ce1))
+print(ce2)
+
+x0=input("enter independent value to find dependent value: ")
+y0=ce1*x0+ce2
+print("dependent value is: {}".format(y0))
+
+
+
+
+
+
