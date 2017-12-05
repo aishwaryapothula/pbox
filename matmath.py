b4e88685fe8be72fad14f8b59eba4faa70e762bd
aishwaryapothula <aishwarya.pothula@gmail.com>
2017-12-05 21:08:14 +0530

    matricies

--- matmath.py
+++ matmath.py
@@ -1,4 +1,5 @@
-#Multiply Matricies also Transverse-becasue matrix be is only 1s
+#AishwaryaPothula
+#Math operation on matricies
 
 
 import numpy
@@ -6,12 +7,16 @@ a=numpy.array([[1,2,3,13],[4,5,6,11],[7,8,9,10],[15,0,4,0]])
 b=numpy.ones((4,4))
 r=numpy.zeros((4,4))
 
-#Multiplication
+#Multiplication logic
 
 for i in range(0,len(a)):
     for j in range(0,len(a)):
         r[i,j]=a[j,i]*b[i,j]
 print(r)
 
-
+#either of the below syntaxes can be used to perform math functions
+print(numpy.add(a,b))
+print(a*r)
+print(r/a)
+print(numpy.subtract(r,a))
 
