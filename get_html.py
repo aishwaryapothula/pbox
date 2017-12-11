f630c048f233d90bdc2da2a29a81a9a9ea10cc49
aishwaryapothula <aishwarya.pothula@gmail.com>
2017-12-11 23:53:53 +0530

    html

--- get_html.py
+++ get_html.py
@@ -1,5 +1,16 @@
+#AishwaryaPothula
+#To get Html code of websites
+
 import requests
+import BeautifulSoup
 url = 'http://facebook.com'
 req = requests.get(url)
 req_html = req.text
 print(req_html)
+
+soup = BeautifulSoup(r_html)
+title = soup.find('span', 'articletitle').string
+print(soup)
+
+#req_html has the html code of facebook in a string format
+#BeautifulSoup convert it into a hierarchical format
